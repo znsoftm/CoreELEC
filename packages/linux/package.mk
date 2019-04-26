26b64bb2e81d919776a881651dedf782b8879a90
adamg <adamg88@users.noreply.github.com>
2019-04-26 10:50:46 +0100

    linux: bump amlogic-4.9 to fd5ea1f (amlogic-4.9-next-2)
    projects/Amlogic-ng: sync linux config with upstream

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -33,8 +33,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   amlogic-4.9)
-    PKG_VERSION="96ab5e1033a6bbc66fb2b2445eeedf755a76211b"
-    PKG_SHA256="b68d5634ca0440fb8e94975a60ec1913eb2c695e8b8c6b387e035375c1aa9680"
+    PKG_VERSION="fd5ea1f7a7c1149268e585fec8a0c8df51285000"
+    PKG_SHA256="b260c55cbd6df21fcfa117369ffd8a58163ae89e8408778fa410db5d0b85d123"
     PKG_URL="https://github.com/CoreELEC/linux-amlogic/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET aml-dtbtools:host"
