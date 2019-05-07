a45a1e731f218103324b14ac8b07e3b75a87d351
Team CoreELEC <team@coreelec.org>
2019-05-08 00:04:49 +0100

    addons: introduce CoreELEC changes

---
+++ packages/addons/service/aml-vnc/package.mk
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="aml-vnc"
+PKG_VERSION="1.0"
+PKG_SHA256=""
+PKG_REV="101"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/kszaq/aml-vnc"
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain libvncserver"
+PKG_SECTION="service"
+PKG_SHORTDESC="Amlogic VNC server"
+PKG_LONGDESC="Amlogic VNC server ($PKG_VERSION) is a Virtual Network Computing (VNC) server for Amlogic devices"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_NAME="Amlogic VNC"
+PKG_ADDON_TYPE="xbmc.service"
+
+makeinstall_target() {
+  :
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
+    cp -P $PKG_BUILD/aml-vnc $ADDON_BUILD/$PKG_ADDON_ID/bin
+}
---
+++ packages/addons/service/aml-vnc/source/default.py
@@ -0,0 +1,2 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team CoreELEC (https://coreelec.org)
