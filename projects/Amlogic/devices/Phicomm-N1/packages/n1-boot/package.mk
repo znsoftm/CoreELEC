17c48ba845595885d14b87a8b9b63ddd9e816b93
RuralHunter <ruralhunter@gmail.com>
2019-10-18 10:30:19 +0800

    clean up

--- projects/Amlogic/devices/Phicomm-N1/packages/n1-boot/package.mk
+++ projects/Amlogic/devices/Phicomm-N1/packages/n1-boot/package.mk
@@ -1,19 +1,15 @@
 PKG_NAME="n1-boot"
 PKG_VERSION="1.0"
 PKG_LICENSE="GPL"
-PKG_DEPENDS_TARGET="toolchain u-boot-tools-aml"
+PKG_DEPENDS_TARGET="toolchain u-boot-tools-aml u-boot-script"
 PKG_TOOLCHAIN="manual"
 
-make_target() {
-  for src in $PKG_DIR/instboot/*autoscript.src ; do
-    $TOOLCHAIN/bin/mkimage -A $TARGET_KERNEL_ARCH -O linux -T script -C none -d "$src" "$(basename $src .src)" > /dev/null
-  done
-}
+PKG_NEED_UNPACK="$PROJECT_DIR/$PROJECT/devices/$DEVICE/bootloader "
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/share/bootloader
-  cp -a $PKG_BUILD/*autoscript $INSTALL/usr/share/bootloader/
-  cp -a $PKG_DIR/instboot/uEnv.ini $INSTALL/usr/share/bootloader/
+  
+  find_file_path bootloader/uEnv.ini && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader/
   for src in $INSTALL/usr/share/bootloader/*.ini ; do
       sed -e "s/@BOOT_LABEL@/$DISTRO_BOOTLABEL/g" \
           -e "s/@DISK_LABEL@/$DISTRO_DISKLABEL/g" \
@@ -24,10 +20,15 @@ makeinstall_target() {
   done
   
   # Always install the update script
-  cp -a $PKG_DIR/instboot/update.sh $INSTALL/usr/share/bootloader/
-  find_file_path bootloader/update.sh && sed -e "s/@KERNEL_NAME@/$KERNEL_NAME/g" \
+  find_file_path bootloader/update.sh && cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
+  sed -e "s/@KERNEL_NAME@/$KERNEL_NAME/g" \
       -e "s/@LEGACY_KERNEL_NAME@/$LEGACY_KERNEL_NAME/g" \
       -e "s/@LEGACY_DTB_NAME@/$LEGACY_DTB_NAME/g" \
       -i $INSTALL/usr/share/bootloader/update.sh
+
+  # Always install the canupdate script
+  if find_file_path bootloader/canupdate.sh; then
+    cp -av ${FOUND_PATH} $INSTALL/usr/share/bootloader
+  fi
       
 }
