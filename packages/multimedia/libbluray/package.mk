c1fc49fdedd1326b0a8c1cb58747310d806c8bce
RuralHunter <ruralhunter@gmail.com>
2020-06-06 09:46:36 +0800

    upgrade libbluray

--- packages/multimedia/libbluray/package.mk
+++ packages/multimedia/libbluray/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libbluray"
-PKG_VERSION="1.0.2"
-PKG_SHA256="6d9e7c4e416f664c330d9fa5a05ad79a3fb39b95adfc3fd6910cbed503b7aeff"
+PKG_VERSION="1.2.0"
+PKG_SHA256="cd41ea06fd2512a77ebf63872873641908ef81ce2fe4e4c842f6035a47696c11"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://www.videolan.org/developers/libbluray.html"
 PKG_URL="http://download.videolan.org/pub/videolan/libbluray/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
